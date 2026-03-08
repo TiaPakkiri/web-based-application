c88362eb476d59cf0317abaac005ca4579c79b29
TiaPakkiri <tiapakkiri@gmail.com>
2026-03-08 13:05:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,43 @@
 
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    full_name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    role = db.Column(db.String(20), nullable=False)
+
+class Campus(db.Model):
+    __tablename__ = "campuses"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+
+class Category(db.Model):
+    __tablename__ = "categories"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+
+class Project(db.Model):
+    __tablename__ = "projects"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(150), nullable=False)
+    short_description = db.Column(db.String(255), nullable=False)
+    detailed_description = db.Column(db.Text, nullable=False)
+    status = db.Column(db.String(50), default="Pending")
+    date_submitted = db.Column(db.DateTime, default=datetime.utcnow)
+
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    campus_id = db.Column(db.Integer, db.ForeignKey("campuses.id"), nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey("categories.id"), nullable=False)
+
+    owner = db.relationship("User", backref="projects")
+    campus = db.relationship("Campus", backref="projects")
+    category = db.relationship("Category", backref="projects")
